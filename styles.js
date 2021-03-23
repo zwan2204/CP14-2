0367c950cecc475ee00b778a10ab4e6667bf6bac
wangzifneg <zifeng.wang@uqconnect.edu.au>
2021-03-23 13:24:34 +0800

    First push

---
+++ styles.js
@@ -0,0 +1,44 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({ 
+  container: { 
+    flex: 1, 
+    justifyContent: "center", 
+    alignItems: "center",
+  },
+
+  logo: {
+    marginBottom: 100
+  },
+
+  inputView:{
+    width:"20%",
+    backgroundColor:"#465881",
+    borderRadius:25,
+    height:50,
+    marginBottom:20,
+    justifyContent:"center",
+    padding:20
+  },
+
+  inputText:{
+    height:50,
+    color:"white"
+  },
+
+  loginBtn:{
+    width:"20%",
+    backgroundColor:"#fb5b5a",
+    borderRadius:25,
+    height:50,
+    alignItems:"center",
+    justifyContent:"center",
+    marginTop:40,
+    marginBottom:10
+  },
+  loginText:{
+    color:"black"
+  }
+
+
+})
