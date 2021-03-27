2b7c86737bdeaf009dedda54ef13153d725b02f8
张笑唯 <zxw971118@gmail.com>
2021-03-27 14:12:49 +1100

    question preview layout finished

--- screens/CriteriaUploading.js
+++ screens/CriteriaUploading.js
@@ -1,6 +1,14 @@
 import React, { useState } from "react";
-import { SafeAreaView, StyleSheet, Text, View, ScrollView } from "react-native";
-import { Avatar, Button, Card, Title, Paragraph } from "react-native-paper";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Platform,
+  FlatList,
+} from "react-native";
+import { Avatar, Button, Card } from "react-native-paper";
 
 import DropDownPicker from "react-native-dropdown-picker";
 
@@ -9,7 +17,40 @@ const CriteriaUploading = () => {
   const [QuestionPrefix, setQuestionPrefix] = useState("");
   const [CriteriaDetail, setCriteriaDetail] = useState("");
   const [OtherInfo, setOtherInfo] = useState("");
-  const LeftContent = (props) => <Avatar.Icon {...props} icon="folder" />;
+
+  const data = [
+    {
+      id: "1",
+      description: "do you smoking every day?",
+    },
+    {
+      id: "2",
+      description: "do you dancing every day?",
+    },
+    {
+      id: "3",
+      description: "are you age below 70?",
+    },
+    {
+      id: "4",
+      description: "are you age below 70?",
+    },
+    {
+      id: "5",
+      description: "are you age below 70?",
+    },
+  ];
+
+  const renderList = (item) => {
+    return (
+      <Card style={styles.mycard} key={item.id}>
+        <View style={styles.cardView}>
+          <Text styl={styles.text}>{item.description}</Text>
+        </View>
+      </Card>
+    );
+  };
+
   return (
     <SafeAreaView style={styles.root}>
       {/*View of Header*/}
@@ -42,67 +83,72 @@ const CriteriaUploading = () => {
       </View>
 
       {/* View of Body*/}
-      <ScrollView style={{ flex: 4, padding: 35 }}>
+      <ScrollView style={{ padding: 35 }}>
         {/* View of "New Project and "Cancel Button*/}
         <View
           style={{
             flex: 1,
             flexDirection: "row",
+            ...(Platform.OS !== "android" && {
+              zIndex: 10,
+            }),
           }}
         >
-          <View style={{ flex: 1 }}>
-            <Text
-              style={{
-                fontWeight: "bold",
-                fontSize: 35,
-                color: "gray",
-                marginBottom: 10,
-              }}
-            >
-              New Project
-            </Text>
-          </View>
-          <View style={{ flex: 1, alignItems: "flex-end" }}>
-            <Button
-              mode="contained"
-              style={{ width: 100 }}
-              onPress={() => console.log("Pessed")}
-            >
-              Cancel
-            </Button>
-          </View>
+          <Text
+            style={{
+              fontWeight: "bold",
+              fontSize: 35,
+              color: "gray",
+              marginBottom: 10,
+            }}
+          >
+            New Project
+          </Text>
+
+          <Button
+            mode="contained"
+            style={{ width: 100, position: "absolute", right: 30 }}
+            onPress={() => console.log("Pessed")}
+          >
+            Cancel
+          </Button>
         </View>
 
         {/* Question input area*/}
-        <View style={{ flex: 10 }}>
-          <View style={{ flexDirection: "row" }}>
+        <View>
+          <View
+            style={{
+              flexDirection: "row",
+              ...(Platform.OS !== "android" && {
+                zIndex: 10,
+              }),
+            }}
+          >
             {/*First input bar*/}
 
-            <View>
-              <DropDownPicker
-                items={[
-                  {
-                    label: "INCLUSION",
-                    value: "INCLUSION",
-                  },
-                  {
-                    label: "EXCLUSION",
-                    value: "EXCLUSION",
-                  },
-                ]}
-                placeholder="Select Type"
-                containerStyle={{ height: 40, width: 140, marginRight: 10 }}
-                style={{ backgroundColor: "#fafafa" }}
-                selectedLabelStyle={{
-                  color: "red",
-                }}
-                itemStyle={{
-                  justifyContent: "flex-start",
-                }}
-                dropDownStyle={{ backgroundColor: "#fafafa" }}
-                onChangeItem={(item) => setCriteriaType(item.value)}
-              />
-            </View>
+            <DropDownPicker
+              items={[
+                {
+                  label: "INCLUSION",
+                  value: "INCLUSION",
+                },
+                {
+                  label: "EXCLUSION",
+                  value: "EXCLUSION",
+                },
+              ]}
+              placeholder="Select Type"
+              containerStyle={{ height: 40, width: 140, marginRight: 10 }}
+              style={{ backgroundColor: "#fafafa" }}
+              selectedLabelStyle={{
+                color: "red",
+              }}
+              itemStyle={{
+                justifyContent: "flex-start",
+              }}
+              dropDownStyle={{ backgroundColor: "#fafafa" }}
+              onChangeItem={(item) => setCriteriaType(item.value)}
+            />
 
             {/*Second input bar*/}
             <DropDownPicker
@@ -180,59 +226,51 @@ const CriteriaUploading = () => {
             />
           </View>
 
-          {/* <View style={{ flexDirection: "row", flex: 1 }}>
+          <View style={{ flexDirection: "row", height: 600 }}>
             <View style={{ flex: 3 }}>
-              <View
-                style={{
-                  flex: 1,
-                  flexDirection: "row",
-                }}
-              >
-                <Text
-                  style={{
-                    position: "absolute",
-                    left: 0,
-                    top: "30%",
-                    fontSize: 20,
-                    color: "#00205B",
+              <View style={{ flex: 1 }}>
+                <View style={{ height: 70 }}>
+                  <Text
+                    style={{
+                      position: "absolute",
+                      left: 0,
+                      top: 30,
+                      fontSize: 20,
+                      color: "#00205B",
+                    }}
+                  >
+                    Quesion Preview:
+                  </Text>
+                  <Button
+                    mode="contained"
+                    style={{
+                      width: 68,
+                      top: 20,
+                      position: "absolute",
+                      right: 0,
+                    }}
+                    onPress={() => console.log("Pessed")}
+                  >
+                    Add
+                  </Button>
+                </View>
+
+                <FlatList
+                  data={data}
+                  renderItem={({ item }) => {
+                    return renderList(item);
                   }}
-                >
-                  Quesion Preview:
-                </Text>
-                <Button
-                  mode="contained"
-                  style={{
-                    width: 68,
-                    top: "20%",
-                    position: "absolute",
-                    right: 0,
-                  }}
-                  onPress={() => console.log("Pessed")}
-                >
-                  Add
-                </Button>
-              </View>
-              <View style={{ flex: 8 }}>
-                <Card>
-                  <Card.Title
-                    title="Card Title"
-                    subtitle="Card Subtitle"
-                    left={LeftContent}
-                  />
-                  <Card.Content>
-                    <Title>Card title</Title>
-                    <Paragraph>Card content</Paragraph>
-                  </Card.Content>
-                  <Card.Cover source={{ uri: "https://picsum.photos/700" }} />
-                  <Card.Actions>
-                    <Button>Cancel</Button>
-                    <Button>Ok</Button>
-                  </Card.Actions>
-                </Card>
+                />
               </View>
             </View>
 
-            <View style={{ flex: 1, padding: 20, marginTop: 100 }}>
+            <View
+              style={{
+                flex: 1,
+                padding: 20,
+                marginTop: 100,
+              }}
+            >
               <View>
                 <Text style={{ color: "gray" }}>
                   *Inclusion Criteria Question:
@@ -250,12 +288,31 @@ const CriteriaUploading = () => {
                 </Text>
               </View>
             </View>
-          </View> */}
+          </View>
         </View>
       </ScrollView>
 
+      <Button
+        mode="contained"
+        style={{ width: 100, alignSelf: "center", marginBottom: 20 }}
+        onPress={() => console.log("Pessed")}
+      >
+        Save
+      </Button>
+
       {/* View of Footer*/}
-      <View style={{ height: 70, backgroundColor: "#00205B" }}></View>
+      <View
+        style={{
+          height: 70,
+          backgroundColor: "#00205B",
+          justifyContent: "center",
+        }}
+      >
+        <Text style={{ color: "white", fontSize: 17, marginLeft: 10 }}>
+          NSW Health website | Disclaimer | Privacy | Copyright | Accessibility
+          | Site map
+        </Text>
+      </View>
     </SafeAreaView>
   );
 };
@@ -269,6 +326,17 @@ const styles = StyleSheet.create({
     justifyContent: "space-around",
     padding: 10,
   },
+  mycard: {
+    margin: 5,
+    padding: 8,
+  },
+  cardView: {
+    flexDirection: "row",
+    padding: 6,
+  },
+
+  text: { fontSize: 20 },
+
   modalView: {
     // position: "absolute",
     // bottom: 1,
