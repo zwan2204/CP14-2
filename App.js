d37a5d722e9b823d6500193d6d718e07b667fa8f
张笑唯 <zxw971118@gmail.com>
2021-04-20 13:25:44 +1000

    change home page to singin

--- App.js
+++ App.js
@@ -8,8 +8,8 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={{ flex: 1 }}>
-        {/* <AppNavigator /> */}
-        <CriteriaUploading />
+        <AppNavigator />
+        {/* <CriteriaUploading /> */}
       </View>
     );
   }
