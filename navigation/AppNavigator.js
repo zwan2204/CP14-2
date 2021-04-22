97a1b10e467fe6a5f25b3146dc04597488dd0f7c
zyao0708 <zyao6449@uni.sydney.edu.au>
2021-04-22 11:40:52 +1000

    questionnaire-page-1-1ed

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -9,9 +9,12 @@ import CriteriaUploading from "../screens/CriteriaUploading";
 import ParticipantPage from "../screens/ParticipantPage";
 import WorkerPage from "../screens/WorkerPage";
 import ParticipantInfoScreen from "../screens/ParticipantInfoScreen"
+import QuestionAnswerPage from "../screens/QuestionAnswerPage"
+import QuestionPreLoadingPage from "../screens/QuestionPreLoadingPage"
 
 export default createAppContainer(
   createSwitchNavigator({
+    QuestionAnswerPage: QuestionAnswerPage,
     Login: Login,
     Signup: Signup,
     CriteriaUploading: CriteriaUploading,
