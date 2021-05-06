0635134eded92c18816f21dc1ab04c7aa9302cce
zyao0708 <zyao6449@uni.sydney.edu.au>
2021-05-06 22:21:21 +1000

    remove

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -2,11 +2,9 @@
 
 import { combineReducers } from "redux";
 import userReducer from "./userReducer";
-import projectReducer from "./projectReducer";
 
 const reducers = combineReducers({
   user: userReducer,
-  projects: projectReducer
 });
 
 export default reducers;
