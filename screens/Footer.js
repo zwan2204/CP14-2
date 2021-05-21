ed74a8e380477ef62a8e3b150639dcc513ff4095
zwan2204 <zwan2204@uni.sydney.edu.au>
2021-05-21 14:55:25 +0800

    Footer

--- screens/Footer.js
+++ screens/Footer.js
@@ -15,13 +15,18 @@ export default class Footer extends React.Component {
         style={{
           height: "5%",
           backgroundColor: "#00205B",
-          justifyContent: "center"
+          justifyContent: "center",
         }}
       >
-        <Text style={{ color: "white", fontSize: 10, marginLeft: 10 }}>
-          NSW Health website | Disclaimer | Privacy | Copyright | Accessibility
-          | Site map
-        </Text>
+        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+          <Text style={{ color: "white", fontSize: 10, marginLeft: 10 }}>
+            NSW Health website | Disclaimer | Privacy | Copyright | Accessibility
+            | Site map
+          </Text>
+          <Text style={{ color: "white", fontSize: 10, marginRight: 10 }}>
+            Contact the Strategic Communications team if you have a question or need some help p. 4920 4000
+          </Text>
+        </View>
       </View>
     );
   }
